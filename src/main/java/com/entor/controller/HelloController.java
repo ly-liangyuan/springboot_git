9480532796b268aa905df0769ba1d2d88084dab6
ly-liangyuan <liangyuanlms@163.com>
2020-11-30 16:27:28 +0800

    1

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello word!";
     }
+
+
 }
